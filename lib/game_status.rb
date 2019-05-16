c8acf0c1c9ec0e58bfdf3b11ec6fa004473ac65f
Andrew Wester <awester17@gmail.com>
2019-05-16 14:08:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,4 +19,7 @@ def won?(board)
     position_3 = board[win_index_3]
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
+    else
+      false
+    end
 end
