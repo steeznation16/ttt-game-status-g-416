cf6f860e96bb6e6ae75915282d6aa8f7473cc23e
Andrew Wester <awester17@gmail.com>
2019-05-16 14:15:02 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,4 +12,5 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
     board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && position_taken?(board, combo[0])
+  end
 end
